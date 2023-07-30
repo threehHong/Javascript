89802255989a89477d7328cef40f6ba14fde29ce
threehHong <honghyohun@naver.com>
2023-07-30 15:16:50 +0900

    class 학습 정리중

---
+++ class/class.js
@@ -0,0 +1,10 @@
+/* function student() {
+    this.name = 'mike';
+    this.studentId = 1212;
+    this.major = 'politics';
+    this.grades = '4.2';
+}
+
+let student1 = new student('tom', 1234, 'education', 4.2);
+
+console.log(student1); */
