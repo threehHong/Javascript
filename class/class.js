ff9f446f03ed318164ea6324e94f7873b8989bd6
threehHong <honghyohun@naver.com>
2023-07-31 18:46:50 +0900

    class 정리 (주석 제거)

--- class/class.js
+++ class/class.js
@@ -12,20 +12,20 @@ new 클래스명(); 형태로 호출한다
 // 브라우저에게 엄격하게 에러를 검토하라고 하는 명령어
 // 'use strict'
 
-// class Person {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-//     }
-//     showInfo() {
-//         console.log(`나의 이름은 ${this.name}이고, 나이는 ${this.age}입니다`);
-//     }
-// }
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    showInfo() {
+        console.log(`나의 이름은 ${this.name}이고, 나이는 ${this.age}입니다`);
+    }
+}
 
-// const a = new Person('Tom', 22);
-// const b = new Person('Jake', 25);
+const a = new Person('Tom', 22);
+const b = new Person('Jake', 25);
 
-// console.log(a, b);
+console.log(a, b);
 
-// console.log(a.showInfo());
+console.log(a.showInfo());
 
