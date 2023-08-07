fe0d9b7f2dda6a0a44ec81bd36882746cbf7eb42
threehHong <honghyohun@naver.com>
2023-08-07 11:46:26 +0900

    예시 코드 및 설명 주석 추가

--- Promise/promise.js
+++ Promise/promise.js
@@ -5,10 +5,11 @@ Promise
 
 2. 프로미스는 3가지 상태를 가질 수 있다. 대기(pending), 이행(resolved), 거부(rejeccted)
 
-3. promise 객체에서 then과 catch를 사용할 수 있다(promise console로 확인해보기).
+3. promise 객체에서 then, catch, finally를 사용할 수 있다(promise console로 확인해보기).
 
 4. fetch 함수가 성공했을 때 then 실행 - then의 콜백 함수의 결과값을 첫번째 파라미터로 받고 그 값을 반환한다 반환한 결과 값은 객체 형태이다(resolved).
    fetch 함수가 실패했을 때 catch 실행(rejected).
+   fetch 함수의 성공과 실패 상관없이 finally 실행.
 
 5. response.json()는 promise를 반환한다 -> 반환한 promise에서 then의 콜백 함수의 결과값을 첫번째 파라미터로 전달받아 출력하면 json 데이터에서 자바스크립트의 데이터로 변환된 데이터를 확인할 수 있다.
  
@@ -17,8 +18,8 @@ Promise
    resolved -  
    rejected - 거부하다, 거절하다.
 *** 콜백 함수를 연속해서 사용할 때 가독성이 떨어지는 문제를 극복할 수 있다(콜백헬로 인한 가독성이 떨어지는 문제를 극복할 수 있다).
-
 */
+
 /* 
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(function(response) {
@@ -36,8 +37,6 @@ console.log(result); */
 
 
 
-
-
 /* 
 Promise - new Promise (사용자 정의 promise)
 
@@ -45,20 +44,41 @@ Promise 함수에서 첫번째 파라미터는 성공했을 떄 실행될 함수
                 두번쨰 파라미터는 실패했을 때 실행될 함수가 온다.
 */
 
+// 1. 
+let americano = true;
 
-// 1.
+const coffeeOrder = new Promise((resolve, reject) => {
+    if(americano) {
+        // resolve의 괄호 안의 문자열을 then에 전달
+        resolve("아메리카노 주문 완료"); 
+    } else {
+        // reject의 괄호 안의 문자열을 catch에 전달
+        reject("아메리카노 주문 실패");
+    }
+});
+
+coffeeOrder
+    .then(result => console.log(result)) // 아메리카노 주문 완료
+    // .catch(reject => console.log(reject)) // 아메리카노 주문 실패
+    .catch(err => console.log(err)) // 아메리카노 주문 실패
+    .finally(() => {
+        console.log('아메라카노 주문 시스템');
+    })
+
+
+// 2.
 /* let job1 = new Promise(function(resolve, reject){});
 console.log(job1); // Promise 객체 출력. */
 
 
-// 2.
+// 3.
 /* let job1 = new Promise(function(resolve, reject){});
 job1.then(function(data) {
     console.log('data', data); // 아무것도 출력되지 않는다.
 }) */
 
 
-// 3.
+// 4.
 /* let job1 = new Promise(function(resolve, reject){
     // job1에 입력될 결과물.
     resolve('resolved ok!');
@@ -68,7 +88,7 @@ job1.then(function(data){
 }) */
 
 
-// 4.
+// 5.
 function job1() {
     return new Promise(function(resolve, reject){
             setTimeout(function() {
@@ -106,3 +126,10 @@ job1()
     .catch(function(reason){
         console.log(reason);
     })
+
+
+
+/* 
+프로미스 체이닝
+return을 사용해 프로미스를 만들어 연결하는 것.
+*/
