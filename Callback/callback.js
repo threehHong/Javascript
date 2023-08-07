2263a47d89ab9c3d1854c4036af4b657f0d80551
threehHong <honghyohun@naver.com>
2023-08-07 11:45:47 +0900

    내용 설명 주석 수정

--- Callback/callback.js
+++ Callback/callback.js
@@ -4,7 +4,11 @@
 함수의 인자로 들어가는 함수.
 
 용도
-순차적으로 실행시키기 위해 사용.
+간단한 내용을 비동기 처리하기 위해 사용하기 적합.
+
+단점
+콜백을 반복해서 사용할 경우 가독성이 떨어지고 오류가 발생했을 때 수정하기 어렵다.
+* callback 함수의 단점으로 인해 promise, async, await 개념이 나왔다.
 
 // ex1
 document.querySelector('.button').addEventListener('click', function(){
@@ -33,11 +37,7 @@ first(second)
 
 
 /* 
-2. Callback 함수의 한계와 극복
-콜백함수로 인한 코드의 복잡함을 극복하기 위해 promise가 나왔다 -> promise로 인한 코드의 복잡함을 극복하기 위해 async & await가 나왔다.
-
-
-3. Callback Hell 
+2. Callback Hell 
 */
 // 함수 정의
 function increaseAndPrint(n, callback) {
