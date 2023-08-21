77cbdf53d75b7cc724b9a9af39dcb7c068f063ca
threehHong <109531669+threehHong@users.noreply.github.com>
2023-08-21 22:00:36 +0900

    문자열을 배열로 변환하는 예시 추가

--- spread operator/spread operator.js
+++ spread operator/spread operator.js
@@ -3,12 +3,19 @@ let obj1= { name:"김철수", age: 38 };
 let obj2 = { ...obj1 }
 obj2.name = "김남일";
 
+
 // 객체에 속성값 변경 
 // 변경을 워하는 값의 키와 변경할 값을 아래와 같이 입력한다.
 let obj2 = { ...obj1, name: "안철수" };
 console.log(obj2); // {name:안철수, age:38}
 
+
 // 객체에 속성 추가
 // 추가를 원하는 키와 값을 아래와 같이 입력한다.
 let obj2 = { ...obj1, gender: "female" };
 console.log(obj2); // {name:안철수, age:38, gender:female}
+
+
+// 문자열을 배열로 변환하는 경우
+const string = "Hello";
+console.log([...string]); // ["H", "e", "l", "l", "o"]
