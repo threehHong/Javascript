c55722ac8b12c1dcfe5472d5cb6ce287f459193a
threehHong <honghyohun@naver.com>
2023-08-26 10:44:27 +0900

    Object.values()

---
+++ method/Object.values.js
@@ -0,0 +1,12 @@
+/* 
+Object.values()
+주어진 객체의 키의 값들로 이루어진 배열을 반환하는 메소드
+*/
+
+const student = {
+  name: "Tom",
+  age: 22,
+  major: "Coumputer Science",
+};
+
+console.log(Object.values(student));
